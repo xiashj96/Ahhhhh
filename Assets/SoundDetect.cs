eed579a622863e55d90eb0f764be5102e609d961
Xia Shijie <xiashj96@gmail.com>
2017-03-04 16:47:46 +0800

    Initial Commit

---
+++ Assets/SoundDetect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundDetect : MonoBehaviour {
+    private AudioClip clip;
+    private string deviceName;
+    public int sampleSize = 128;
+    public float maxVolume;
+    public SoundVisualize sv;
+    private float sum_old = 0;
+	// Use this for initialization
+	void Start () {
+        deviceName = Microphone.devices[0];
+        Debug.LogFormat("Number of devices: {0}", Microphone.devices.Length);
+        clip = Microphone.Start(deviceName, true, 10, 44100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int startPos = Microphone.GetPosition(deviceName) - (sampleSize + 1);
+        float[] data = new float[sampleSize];
+        clip.GetData(data, Mathf.Max(0,startPos));
+
+        float sum = 0f;
+        foreach (float sample in data)
+        {
+            sum += Mathf.Abs(sample);
+        }
+        // 平滑音量信号
+        float volume = Mathf.Lerp(sum_old, sum, 0.2f);
+        sum_old = volume;
+
+        sv.DisplayVolume(Mathf.Clamp(volume, 0, maxVolume) / maxVolume);
+	}
+}
---
+++ Assets/SoundVisualize.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundVisualize : MonoBehaviour {
+
+    public Color volumeColor;
+    private float height;
+
+    void Awake()
+    {
+        height = transform.localScale.y;
+    }
+
+    /// <summary>
+    /// display the volume of current microphone as a bar
+    /// </summary>
+    /// <param name="Volume">a float number in [0,1]</param>
+	public void DisplayVolume(float volume)
+    {
+        transform.localScale = new Vector3(transform.localScale.x, volume * height, transform.localScale.z);
+    }
+}
